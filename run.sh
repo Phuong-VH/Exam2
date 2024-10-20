7ce09c117570623428a1ae9fb4d71f3f69fe09b5
PhuongVH <vanhoangphuong1610@gmail.com>
2024-10-21 01:21:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ python3 -m pip install --upgrade pip
 pip3 install catboost
 pip3 install --upgrade tensorflow
 pip3 install --upgrade numpy pandas sklearn pillow;
-python3 ./PV_Test2/CNN_PSO.py;
+python3 ./Exam2/CNN_PSO.py;
