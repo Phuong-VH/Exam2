08fe722915d68c3c9fd3546d498543a6cd7e9d5b
PhuongVH <vanhoangphuong1610@gmail.com>
2024-10-21 15:27:22 +0800

    Add files via upload

--- CNN_PSO.py
+++ CNN_PSO.py
@@ -1,22 +1,27 @@
-# from __future__ import print_function, division
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-# from tensorflow.python.client import device_lib
-# print(device_lib.list_local_devices())
-# import tensorflow as tf
+from __future__ import print_function, division
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,0"
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, Dense, Dropout, BatchNormalization
 from keras.layers import Input, ZeroPadding3D, concatenate
-from tensorflow.keras import regularizers
+from tensorflow.keras import regularizers, Model
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 from qpso import QPSO
 import datetime
 from keras.utils import to_categorical
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import numpy as np
+from catboost import CatBoostClassifier
+
+# Use MirroredStrategy for distributed training across multiple GPUs
+strategy = tf.distribute.MirroredStrategy()
+
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
 data_directory = './PV_Test/3D_data_total.npz'
 data = np.load(data_directory)
@@ -24,7 +29,7 @@ data = np.load(data_directory)
 X = data['X_all']
 Y = data['Y_all']
 
-X_all = X/255
+X_all = X / 255
 
 X_train, X_test, y_train, y_test = train_test_split(X_all, Y, test_size=0.20, random_state=42)
 
@@ -34,7 +39,7 @@ y_test_onehot = to_categorical(y_test)
 
 # Convert the target labels to dense tensors
 y_train_dense = tf.convert_to_tensor(y_train_onehot, dtype=tf.float32)
-
+y_test_dense = tf.convert_to_tensor(y_test_onehot, dtype=tf.float32)
 def first_model(zeropad_1, filt1, filtlen_1,dropout1,pol1):
     nb_filter1  = filt1;
     filter1_size = filtlen_1;
@@ -66,7 +71,7 @@ def second_model(zeropad_1, filt1, filt2, filtlen_1, filtlen_2, dropout1, dropou
     flat2 = Flatten()(drop2)
     return flat2
 
-def thrid_model(zeropad_1, filt1, filt2, filt3, filtlen_1, filtlen_2, filtlen_3, dropout1, dropout2, dropout3, pol1, pol2, pol3):
+def third_model(zeropad_1, filt1, filt2, filt3, filtlen_1, filtlen_2, filtlen_3, dropout1, dropout2, dropout3, pol1, pol2, pol3):
     nb_filter1  = filt1;
     nb_filter2  = filt2;
     nb_filter3  = filt3;
@@ -83,11 +88,9 @@ def thrid_model(zeropad_1, filt1, filt2, filt3, filtlen_1, filtlen_2, filtlen_3,
     
     conv1 = Conv3D(nb_filter1, (filter1_size, filter1_size, filter1_size), activation='relu', padding="same")(zeropad_1)
     max1 = MaxPooling3D(pool_size=(poling1, poling1, poling1), padding="same")(conv1)
-    drop1 = Dropout(dropout1)(max1)
-    conv2 = Conv3D(nb_filter2, (filter2_size, filter2_size, filter2_size), activation='relu', padding="same")(drop1)
+    conv2 = Conv3D(nb_filter2, (filter2_size, filter2_size, filter2_size), activation='relu', padding="same")(max1)
     max2 = MaxPooling3D(pool_size=(poling2, poling2, poling2), padding="same")(conv2)
-    drop2 = Dropout(dropout2)(max2)
-    conv3 = Conv3D(nb_filter3, (filter3_size, filter3_size, filter3_size), activation='relu', padding="same")(drop2)
+    conv3 = Conv3D(nb_filter3, (filter3_size, filter3_size, filter3_size), activation='relu', padding="same")(max2)
     max3 = MaxPooling3D(pool_size=(poling3, poling3, poling3), padding="same")(conv3)
     drop3 = Dropout(dropout3)(max3)    
     flat3 = Flatten()(drop3)
@@ -178,7 +181,7 @@ def cnn(x):
     elif struct == 2:
         nn = second_model(zeropad_1, filt1, filt2, filtlen_1, filtlen_2, dropout1, dropout2, pol1, pol2)
     elif struct == 3:
-        nn = thrid_model(zeropad_1, filt1, filt2, filt3, filtlen_1, filtlen_2, filtlen_3, dropout1, dropout2, dropout3, pol1, pol2, pol3)
+        nn = third_model(zeropad_1, filt1, filt2, filt3, filtlen_1, filtlen_2, filtlen_3, dropout1, dropout2, dropout3, pol1, pol2, pol3)
     elif struct == 4:
         nn = fourth_model(zeropad_1, filt1, filt2, filtlen_1, filtlen_2, dropout1, dropout2, pol1, pol2)
     elif struct == 5:
@@ -188,27 +191,31 @@ def cnn(x):
 
     dropout_output = Dropout(dropout4) (nn)
     hid = Dense(dense1, activation='relu')(dropout_output)
-    output = Dense(10, activation='softmax')
+    output = Dense(10, activation='softmax')(hid)
+    
+    model = Model(inputs=inp_1, outputs=output)
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    cnn_model = model.fit(X_train, y_train_dense, epochs=100, batch_size=16, verbose=0, validation_data=(X_test, y_test_dense))
-    cnn_features = model.predict(X_train)
-    y_train_dense_np = np.array(y_train_dense)
-    y_train_catboost = np.argmax(y_train_dense_np, axis=1)
+    cnn_model = model.fit(X_train, y_train_dense, epochs=100, batch_size=1, verbose=0, validation_data=(X_test, y_test_dense))
     
-    catboost_model = CatBoostClassifier(iterations=catboost_iterations, depth=catboost_depth, learning_rate=catboost_learning_rate, verbose=0)
-    catboost_model.fit(cnn_features, y_train_catboost)
-    
-    score = catboost_model.score(cnn_features, y_train_catboost)
+    score = cnn_model.history['accuracy'][-1]
 
     t2_0 = datetime.datetime.now()
     
-    print('score: ', -score, 'values: ', func, 'time: ', t2_0-t1_0)
+    print('score: ', score, 'values: ', x, 'time: ', t2_0-t1_0)
     return -score 
+def log(s):
+    best_value = [p.best_value for p in s.particles()]
+    best_value_avg = np.mean(best_value)
+    best_value_std = np.std(best_value)
+    print("{0: >5}  {1: >9}  {2: >9}  {3: >9}".format("Iters.", "Best", "Best(Mean)", "Best(STD)"))
+    print("{0: >5}  {1: >9.3E}  {2: >9.3E}  {3: >9.3E}".format(s.iters, s.gbest_value, best_value_avg, best_value_std))
 
-bounds = [(1,5),(16,64),(16,64),(16,64),(2,5),(2,5),(2,5),(0.2,0.5),(0.2,0.5),(0.2,0.5),(2,5),(2,5),(2,5),(32,128),(0.2,0.5)]
+bounds = [(1,5),(8,32),(8,32),(8,32),(2,5),(2,5),(2,5),(0.2,0.5),(0.2,0.5),(0.2,0.5),(2,5),(2,5),(2,5),(32,128),(0.2,0.5)]
 NDim = 15;
-s = QPSO(sphere, NParticle, NDim, bounds, MaxIters)
+NParticle =NDim*3
+MaxIters=50
+s = QPSO(cnn, NParticle, NDim, bounds, MaxIters)
 s.update(callback=log, interval=100)
 print("Found best position: {0}".format(s.gbest))
 
